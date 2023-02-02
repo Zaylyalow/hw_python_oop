31c5e04518feb041ed79905675413fe693257780
Shamil Zaylyalov <Zaylyalow@gmail.com>
2023-02-02 20:13:56 +0500

    done_1.9

--- homework.py
+++ homework.py
@@ -11,11 +11,11 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    TEXT: str = ('Тип тренировки: {training_type}: '
-                 'Длительность: {duration:.3f} ч, '
-                 'Дистанция: {distance:.3f} км, '
-                 'Ср. скорость: {speed:.3f} км/ч, '
-                 'Потрачено ккал: {calories:.3f}. ')
+    TEXT: str = ('Тип тренировки: {training_type}; '
+                 'Длительность: {duration:.3f} ч.; '
+                 'Дистанция: {distance:.3f} км; '
+                 'Ср. скорость: {speed:.3f} км/ч; '
+                 'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         return self.TEXT.format(**asdict(self))
